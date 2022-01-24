f7908ad29dcbd0055224338418cfcf978bb83a60
Maeg <kimhj5854@naver.com>
2022-01-24 23:50:54 +0900

    220124 Node.js 6강

--- 07_app.js
+++ 07_app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 var app = express(); // application을 리턴한다.
 
+app.use(express.static('public')); // public 디렉토리를 정적파일 제공 디렉토리로 지정.
+
+app.get('/dynamic', function(req, res){
+    var lis = '';
+    for (var i=0; i<5 ;i++){
+        lis = lis + '<li>coding</li>';
+    }
+    var output = `<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>
+                my homepage
+            </title>
+            <body>
+                hello, static!
+                ${lis}
+            </body>
+        </head>
+    </html>`;
+    res.send(output);
+});
 app.get('/', function(req, res){
-    res.send();
+    res.send('hello router,<img src="시간표.png">'); // public 디렉토리 내의 정적 파일을 바로 접근한다.
 });
 
 app.listen(3000,function(){
